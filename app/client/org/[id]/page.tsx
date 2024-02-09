1e7f5763552b572ad10359e7edef84bc8f04c731
Jo1ceLi <ms0615122@gmail.com>
2024-02-09 20:09:53 +0800

    Refactor OrgID component to fetch and display credit card information

--- app/client/org/[id]/page.tsx
+++ app/client/org/[id]/page.tsx
@@ -1,15 +1,27 @@
 import { cookies } from "next/headers";
 import CreateCard from "../../components/create-card-card";
 import { redirect } from "next/navigation";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 
-export default function OrgID({ params }: { params: any }) {
+interface Card {
+  id: string;
+  alias: string;
+  funding: string;
+  network: string;
+  last_four: string;
+  expiry: string;
+  rank: string;
+}
+
+export default async function OrgID({ params }: { params: any }) {
   const createcardaction = async (prime: string) => {
     "use server";
     const token = cookies().get("token");
     const oid = params["id"];
     if (token && oid) {
       const res = await fetch(
-        `${process.env.BACKEND_HOST}/api/client/org/${oid}/card`,
+        `${process.env.BACKEND_HOST}/api/client/org/${oid}/cards`,
         {
           method: "POST",
           body: JSON.stringify({
@@ -31,22 +43,54 @@ export default function OrgID({ params }: { params: any }) {
       }
     }
   };
+
+  const getCards = async () => {
+    const token = cookies().get("token");
+    const oid = params["id"];
+    if (token && oid) {
+      const res = await fetch(
+        `${process.env.BACKEND_HOST}/api/client/org/${oid}/cards`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token.value,
+          },
+        }
+      );
+      if (res.ok) {
+        const cards = (await res.json()) as Card[];
+        return cards;
+      }
+    }
+  };
+
+  const cards = await getCards();
   return (
     <>
       <div className="container mx-auto">
         HELLO FROM ORG {params.id}
-        <div className="grid lg:grid-cols-3 gap-4">
+        <div className="grid lg:grid-cols-2 gap-4">
           <CreateCard createcardaction={createcardaction} />
-          <div className="relative  bg-slate-700">
-            <div className="bg-yellow-700	absolute left-0 top-0 h-16 w-16 ...">
-              01
-            </div>
-          </div>
-          <div className="relative  bg-slate-700">
-            <div className="bg-yellow-700	absolute left-0 top-0 h-16 w-16 ...">
-              02
-            </div>
-          </div>
+          {cards?.map((c) => {
+            return (
+              <Card key={c.id}>
+                <CardHeader>
+                  <CardTitle>信用卡</CardTitle>
+                </CardHeader>
+                <CardContent className="grid grid-cols-2 gap-4">
+                  <Label className="font-bold">發卡網路</Label>
+                  <div className="text-slate-600">{c.network}</div>
+                  <Label className="font-bold">末四碼</Label>
+                  <div className="text-slate-600">{c.last_four}</div>
+                  <Label className="font-bold">卡種</Label>
+                  <div className="text-slate-600"> {c.funding}</div>
+                  <Label className="font-bold">到期年月</Label>
+                  <div className="text-slate-600">{c.expiry}</div>
+                </CardContent>
+              </Card>
+            );
+          })}
+          <Card></Card>
         </div>
       </div>
     </>
