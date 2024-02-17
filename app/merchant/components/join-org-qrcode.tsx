096c1e8b3d86dca0b2a7f34061b5163d0324262f
Jo1ceLi <ms0615122@gmail.com>
2024-02-18 01:08:58 +0800

    Add JoinOrgQRCode component and integrate it in MerchantCustomer page

---
+++ app/merchant/components/join-org-qrcode.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useQRCode } from "next-qrcode";
+import { usePathname } from "next/navigation";
+
+export default function JoinOrgQRCode(props: { hostname: string }) {
+  const { Canvas } = useQRCode();
+
+  const params = usePathname();
+  console.log(props.hostname + params);
+  return (
+    <Canvas
+      text={props.hostname + params}
+      options={{
+        errorCorrectionLevel: "M",
+        margin: 3,
+        scale: 4,
+        width: 200,
+        color: {
+          dark: "#091e49",
+          light: "#d1d4da",
+        },
+      }}
+    />
+  );
+}
--- app/merchant/orgs/[id]/customers/page.tsx
+++ app/merchant/orgs/[id]/customers/page.tsx
@@ -1,5 +1,15 @@
+import JoinOrgQRCode from "@/app/merchant/components/join-org-qrcode";
 import { MerchantCRM } from "@/components/merchant-crm";
+import { headers } from "next/headers";
 
 export default async function MerchantCustomer() {
-  return <MerchantCRM />;
+  const headersList = headers();
+  const hostname = headersList.get("x-forwarded-host");
+  const protocol = headersList.get("x-forwarded-proto") || "http";
+  return (
+    <>
+      <MerchantCRM />
+      <JoinOrgQRCode hostname={protocol + "://" + hostname!} />
+    </>
+  );
 }
