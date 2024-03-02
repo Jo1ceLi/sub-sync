322a8a21f0c15f881e78368f8dafed869b5e8ed4
Jo1ceLi <ms0615122@gmail.com>
2024-03-02 20:58:51 +0800

    Refactor join URL generation in JoinOrgQRCode component

--- app/merchant/components/join-org-qrcode.tsx
+++ app/merchant/components/join-org-qrcode.tsx
@@ -2,15 +2,21 @@
 
 import { useQRCode } from "next-qrcode";
 import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 export default function JoinOrgQRCode(props: { hostname: string }) {
   const { Canvas } = useQRCode();
 
-  const params = usePathname();
-  console.log(props.hostname + params);
+  const pathname = usePathname();
+
+  const joinURL = useMemo(() => {
+    const orgId = pathname.split("/orgs/")[1].split("/")[0];
+    return `/client/orgs/${orgId}/join`;
+  }, [pathname]);
+
   return (
     <Canvas
-      text={props.hostname + params}
+      text={props.hostname + joinURL}
       options={{
         errorCorrectionLevel: "M",
         margin: 3,
